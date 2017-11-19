6f2c5c5c207d1218527445a1438d7b7b75c67d4e
bearium <jacob_martin@rogers.com>
2017-11-18 22:18:45 -0500

    Creates row items from an object given and creates new table entry

--- wecameforthefood-client/src/App.js
+++ wecameforthefood-client/src/App.js
@@ -1,22 +1,43 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function getlist() {
-  return Promise.resolve([{id: "fdjsaf", date: 32432423}, {id: "fdjsaf", date: 32432423}])
+function getList() {
+  return Promise.resolve([{id: "fdjsaf", expire: 32432423,name: "food", price: 12,size: 20}, {id: "fdjsaf", expire: 32432423,name: "water", price: 12,size: 20}])
 
 }
 
 
+function createRow(item) {
+  return (<tr>
+    <td>{item.name}</td>
+    <td>{item.price}</td>
+    <td>{item.size}</td>
+    <td>{item.expire}</td>
+  </tr>)
 
+
+}
 
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {list: []}
+  }
+
+  componentDidMount() {
+    getList().then((list) => {
+      this.setState({list: list})
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo"/>
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
@@ -24,23 +45,17 @@ class App extends Component {
         </p>
 
         <table border="1">
-        <tbody>
-        <tr>
-        <td>Item</td>
-        <td>Price</td>
-        <td>Volume</td>
-        <td>Expiry Date</td>
-        </tr>
-        </tbody>
+          <tbody>
+          <tr>
+            <td>Item</td>
+            <td>Price</td>
+            <td>Volume</td>
+            <td>Expiry Date</td>
+          </tr>
+
+          {this.state.list.map(createRow)}
+          </tbody>
         </table>
-
-
-
-
-
-
-
-
       </div>
 
     );
